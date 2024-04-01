3d771a9d23c26a583e1c35f9f35c537114b5cf5f
Charlotte Studio <charlottexprojects@gmail.com>
2024-04-01 16:47:22 +0800

    update PIR sample

--- Components/PIR.py
+++ Components/PIR.py
@@ -2,6 +2,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 led = 5
+# pin 11 先可以正常用來 input, pin 3 要上拉成 1
 pir = 11
 
 GPIO.setwarnings(False)
@@ -12,7 +13,8 @@ GPIO.output(led, False)
 GPIO.setup(pir, GPIO.IN)
 
 moved = 0
-coolDownTime = 0;
+coolDownTime = 0
+lightTurnOnTime = 6;
 
 sleep(2) #give sensor to startup
 
@@ -24,12 +26,12 @@ try:
     if moved == 0:
       if coolDownTime <= 0:
         print("Motion detected!")
-      coolDownTime = 6
+      coolDownTime = lightTurnOnTime  # turn on LED 
 
     if coolDownTime > 0:
       GPIO.output(led, True)
     else:
-      GPIO.output(led, False)
+      GPIO.output(led, False)         # everytime detect will reset the cool down time
 
     sleep(0.1)
     coolDownTime -= 0.1
